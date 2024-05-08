08c2ab8fbd359667c187a1201e1c37bdc6d31757
Richardbrunei <48155796+Richardbrunei@users.noreply.github.com>
2024-05-07 21:33:46 -0500

    Add files via upload
    Signed-off-by: Richardbrunei <48155796+Richardbrunei@users.noreply.github.com>

---
+++ codeforces/1944/A/brigdestoy.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int asdfghjkl;
+	cin>>asdfghjkl;
+	for(int qwertyuiop=0;qwertyuiop<asdfghjkl;qwertyuiop++)
+	{
+		int n,k;
+		cin>>n>>k;
+		//cout<<"|||||||||||||||";
+		if(k>=n-1)
+		{
+			cout<<"1\n";
+		}
+		else
+		{
+			cout<<n<<endl;
+		}
+	}
+}
